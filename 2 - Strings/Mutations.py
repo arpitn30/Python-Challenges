5d458250b08a3ed3bffd57680d1ea51d6b65ac47
Arpit <arpit.nandwani@gmail.com>
2017-02-17 00:04:36 +0530

    Added Introduction to Sets

--- 2 - Strings/Mutations.py
+++ 2 - Strings/Mutations.py
@@ -3,7 +3,7 @@
 def mutate_string(string, position, character):
     string = string[:position] + character + string[position+1:]
     # Or can be done by type casting to list and using join after changing the character
-    return string
+    #     return string
 
 if __name__ == '__main__':
     s = input('Enter Text:')
---
+++ 3 - Sets/Introduction.py
@@ -0,0 +1,16 @@
+# A set is an unordered collection of elements without duplicate entries.
+# When printed, iterated or converted into a sequence, its elements will appear in an random order.
+# Print the avg of the distinct elements of all the elements given by the user
+
+
+def average(array):
+    distinct = set(arr)
+    add = sum(distinct)
+    length = len(distinct)
+    return add / length
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    result = average(arr)
+    print(result)
